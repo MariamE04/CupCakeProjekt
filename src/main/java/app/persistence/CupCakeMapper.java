08b5b2ddd26f553600525897813576020265f9e7
Mariam <elmirmariam16@gmail.com>
2025-03-25 14:04:56 +0100

    tilføjet ConnectionPool

--- src/main/java/app/persistence/CupCakeMapper.java
+++ src/main/java/app/persistence/CupCakeMapper.java
@@ -1,6 +1,8 @@
 package app.persistence;
 
+import java.sql.Connection;
+
 public class CupCakeMapper {
 
-    public static int SignUp(String email, String Password, double balance, Con)
+    public static int SignUp(String email, String Password, double balance, Connection)
 }
