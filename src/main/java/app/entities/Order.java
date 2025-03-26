48cdfbb84823791f07f0fb229b7565d071ac04cd
Asger191 <ajunker@outlook.dk>
2025-03-26 10:28:53 +0100

    Buy sat ind

---
+++ src/main/java/app/entities/Order.java
@@ -0,0 +1,19 @@
+package app.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+    List<Cupcake> cupcakes;
+
+    public Order(List<Cupcake> cupcakes){
+        this.cupcakes = new ArrayList<>();
+
+    }
+
+}
+
+
+
+
+
---
+++ src/main/java/app/persistence/CupcakeMapper.java
@@ -0,0 +1,41 @@
+package app.persistence;
+
+import app.entities.Cupcake;
+import app.exceptions.DatabaseException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public class CupcakeMapper {
+
+    public void buyCupcake(Order order, int price, int balance, String email, ConnectionPool connectionPool) throws DatabaseException {
+        String sql = "UPDATE users SET balance = ? WHERE email = ?";
+
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)){
+
+            for (Cupcake cupcake : order.getList()) {
+                price += price;
+            }
+
+            ps.setInt(1, price - balance);
+            ps.setString(2, email);
+
+
+        } catch (SQLException e){
+            throw new DatabaseException("FEEEEJL", e.getMessage());
+        }
+
+
+    }
+
+}
+
+
+
+
+
+
+
