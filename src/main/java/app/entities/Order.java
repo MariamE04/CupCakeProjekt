aeb9ab59d8d745c8c1ea53592606450b6cd9e4bc
Asger191 <ajunker@outlook.dk>
2025-03-26 10:39:13 +0100

    Buy sat ind

--- src/main/java/app/entities/Order.java
+++ src/main/java/app/entities/Order.java
@@ -1,15 +1,16 @@
 package app.entities;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
     List<Cupcake> cupcakes;
 
-    public Order(List<Cupcake> cupcakes){
+    public Order(List<Cupcake> cupcakes, int order_nr, User user, LocalDate localDate){
         this.cupcakes = new ArrayList<>();
-
     }
+
 
 }
 
