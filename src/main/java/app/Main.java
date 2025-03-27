e99c39244b3c947a6b7b5c24c86a4fde5e2ddf70
DanielBasco <danielsgeniale@gmail.com>
2025-03-27 12:29:52 +0100

    Daniel | Lavet get routes til order.html, startpage.html og createcupcake.html.
    Har lavet order.html men thymeleaf skal justeres når orderMapper er færdig, samt når vi har lavet currentUser
    
    Har lavet startpage.html men thymeleaf skal justeres når vi har lavet currentUser
    
    Createcupcake mangles stadig fuldt ud

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,10 +2,9 @@ package app;
 
 import app.config.SessionConfig;
 import app.config.ThymeleafConfig;
-<<<<<<< HEAD
+
 import app.exceptions.DatabaseException;
-=======
->>>>>>> a79232a515b55f28761a4e17d682f08770b50367
+
 import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinThymeleaf;
@@ -18,26 +17,23 @@ import static app.mappers.CupcakeMapper.getTopping;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-<<<<<<< HEAD
+
     private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
-=======
->>>>>>> a79232a515b55f28761a4e17d682f08770b50367
+
     private static final String USER = "postgres";
     private static final String PASSWORD = "postgres";
     private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
     private static final String DB = "cupcake";
 
     private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
-<<<<<<< HEAD
 
-    public static void main(String[] args)
-    {
-=======
+
     public static void main(String[] args) {
->>>>>>> a79232a515b55f28761a4e17d682f08770b50367
-        // Initializing Javalin and Jetty webserver
-        /*
+
+
+            // Initializing Javalin and Jetty webserver
+
         Javalin app = Javalin.create(config -> {
             config.staticFiles.add("/public");
             config.jetty.modifyServletContextHandler(handler ->  handler.setSessionHandler(SessionConfig.sessionConfig()));
@@ -47,13 +43,17 @@ public class Main {
         // Routing
 
         app.get("/", ctx ->  ctx.render("index.html"));
-        */
+        app.get("order", ctx -> ctx.render("order.html"));
+        app.get("startpage", ctx -> ctx.render("startpage.html"));
+        app.get("createcupcake", ctx -> ctx.render("createcupcake.html"));
 
-        try{
-            System.out.println(getBottom(connectionPool));
-        } catch (DatabaseException e) {
-            System.out.println(e.getMessage());
+
+
+            try {
+                System.out.println(getBottom(connectionPool));
+            } catch (DatabaseException e) {
+                System.out.println(e.getMessage());
+            }
+
         }
-
     }
-}
